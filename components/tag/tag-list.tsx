971c15522d4b0a1048366c31d180c5db70420192
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-02-12 12:50:08 -0500

    feat: add visual cues for drag-and-drop on tag list

--- components/tag/tag-list.tsx
+++ components/tag/tag-list.tsx
@@ -11,6 +11,10 @@ export type TagListProps = {
   onSortEnd: (oldIndex: number, newIndex: number) => void;
 } & Omit<TagProps, "tagObj">;
 
+const DropTarget: React.FC = () => {
+  return <div className={cn("h-full rounded-md bg-secondary/50")} />;
+};
+
 export const TagList: React.FC<TagListProps> = ({
   tags,
   customTagRenderer,
@@ -19,6 +23,16 @@ export const TagList: React.FC<TagListProps> = ({
   onSortEnd,
   ...tagListProps
 }) => {
+  const [draggedTagId, setDraggedTagId] = React.useState<string | null>(null);
+
+  const handleMouseDown = (id: string) => {
+    setDraggedTagId(id);
+  };
+
+  const handleMouseUp = () => {
+    setDraggedTagId(null);
+  };
+
   return (
     <div
       className={cn("rounded-md max-w-[450px]", {
@@ -30,11 +44,21 @@ export const TagList: React.FC<TagListProps> = ({
         <SortableList
           onSortEnd={onSortEnd}
           className="flex flex-wrap gap-2 list"
-          // dropTarget={<DropTarget />}
+          dropTarget={<DropTarget />}
         >
           {tags.map((tagObj) => (
             <SortableItem key={tagObj.id}>
-              <div>
+              <div
+                onMouseDown={() => handleMouseDown(tagObj.id)}
+                onMouseLeave={handleMouseUp}
+                className={cn(
+                  {
+                    "border border-solid border-primary rounded-md":
+                      draggedTagId === tagObj.id,
+                  },
+                  "transition-all duration-200 ease-in-out"
+                )}
+              >
                 {customTagRenderer ? (
                   customTagRenderer(tagObj)
                 ) : (
