7c6012aec5580f60bd7ad863a77c2427bf4fc3a0
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-04-27 10:16:58 -0400

    fix(website): added permanent redirect for old url

--- website/next.config.js
+++ website/next.config.js
@@ -1,18 +1,18 @@
-const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "https://emblor.jaleelbennett.com",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: 'https://shadcn-tag-input.vercel.app/',
+        destination: 'https://emblor.jaleelbennett.com',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
