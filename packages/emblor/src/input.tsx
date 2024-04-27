9e6c31bedc60a7ff3fe8b1ff2deee58313b0d843
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-04-27 00:18:59 -0400

    fix(package): extend react.cssproperties to support custom css properties

--- packages/emblor/src/input.tsx
+++ packages/emblor/src/input.tsx
@@ -1,33 +1,40 @@
-import * as React from 'react';
+import * as React from "react";
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const defaultStyle: React.CSSProperties = {
-  '--input-height': '40px',
-  '--input-width': '100%',
-  '--border-radius': '0.375rem',
-  '--padding-horizontal': '0.75rem',
-  '--padding-vertical': '0.5rem',
-  '--font-size': '0.875rem',
-  '--outline-offset': '2px',
-  '--cursor': 'text',
-  '--opacity': '1',
-  display: 'flex',
-  height: 'var(--input-height)',
-  width: 'var(--input-width)',
-  borderRadius: 'var(--border-radius)',
-  paddingLeft: 'var(--padding-horizontal)',
-  paddingRight: 'var(--padding-horizontal)',
-  paddingTop: 'var(--padding-vertical)',
-  paddingBottom: 'var(--padding-vertical)',
-  fontSize: 'var(--font-size)',
-  cursor: 'var(--cursor)',
-  opacity: 'var(--opacity)',
+type ExtendedCSSProperties = React.CSSProperties & {
+  [key: string]: string | number;
 };
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-  return <input type={type} className={className} ref={ref} {...props} />;
-});
-Input.displayName = 'Input';
+const defaultStyle: ExtendedCSSProperties = {
+  "--input-height": "40px",
+  "--input-width": "100%",
+  "--border-radius": "0.375rem",
+  "--padding-horizontal": "0.75rem",
+  "--padding-vertical": "0.5rem",
+  "--font-size": "0.875rem",
+  "--outline-offset": "2px",
+  "--cursor": "text",
+  "--opacity": "1",
+  display: "flex",
+  height: "var(--input-height)",
+  width: "var(--input-width)",
+  borderRadius: "var(--border-radius)",
+  paddingLeft: "var(--padding-horizontal)",
+  paddingRight: "var(--padding-horizontal)",
+  paddingTop: "var(--padding-vertical)",
+  paddingBottom: "var(--padding-vertical)",
+  fontSize: "var(--font-size)",
+  cursor: "var(--cursor)",
+  opacity: "var(--opacity)",
+};
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return <input type={type} className={className} ref={ref} {...props} />;
+  },
+);
+Input.displayName = "Input";
 
 export { Input };
