affbb9d8e79ec217e2278433d7c4358f2c7b4c19
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-04-27 10:57:36 -0400

    feat(package): input code

--- packages/emblor/src/index.ts
+++ packages/emblor/src/index.ts
@@ -1,1 +1,1 @@
-export * from './input';
+export * from './tag/tag-input';
--- website/next.config.js
+++ website/next.config.js
@@ -4,15 +4,6 @@ const { withContentlayer } = require('next-contentlayer');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: 'https://shadcn-tag-input.vercel.app/',
-        destination: 'https://emblor.jaleelbennett.com',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = withContentlayer(nextConfig);
