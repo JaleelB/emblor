a88db146291163917e411c9f5e1028b723d13bb8
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-04-28 22:24:51 -0400

    feat(docs): docs content page

--- website/components/mdx-components.tsx
+++ website/components/mdx-components.tsx
@@ -102,18 +102,15 @@ const components = {
       />
     );
   },
-  // code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-  //   // <code
-  //   //   className={cn(
-  //   //     "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm",
-  //   //     className
-  //   //   )}
-  //   //   {...props}
-  //   // />
-  //   <CodeBlock value={props.children as string} />
-  // ),
   code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <code className={cn('relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm', className)} {...props} />
+    // <code
+    //   className={cn(
+    //     "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm",
+    //     className
+    //   )}
+    //   {...props}
+    // />
+    <CodeBlock value={props.children as string} />
   ),
   Image,
   Step: ({ className, ...props }: React.ComponentProps<'h3'>) => (
