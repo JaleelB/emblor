9593f7cc7bbc6df1e8290bdaca4e6ada4cd7f5d0
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-04-28 20:30:01 -0400

    feat(website): docs page layout

---
+++ website/types/nav.ts
@@ -0,0 +1,16 @@
+import { Icons } from '@/components/icons';
+
+export interface NavItem {
+  title: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: keyof typeof Icons;
+  label?: string;
+}
+
+export interface NavItemWithChildren extends NavItem {
+  items: NavItemWithChildren[];
+}
+
+export interface SidebarNavItem extends NavItemWithChildren {}
