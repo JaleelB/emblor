7cfd13368b3659c1fa0118d1367fb93eab979e6e
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-04-29 21:58:38 -0400

    feat(website): made sitemap dynamic

--- website/app/sitemap.ts
+++ website/app/sitemap.ts
@@ -1,11 +1,18 @@
 import { type MetadataRoute } from 'next';
 import { siteConfig } from '../config/site-config';
+import { docsConfig } from '@/config/docs-config';
+
+const siteUrls = docsConfig.sidebarNav.reduce((acc: MetadataRoute.Sitemap, section) => {
+  section.items.forEach((item) => {
+    const href = item.href === '/' ? '/introduction' : item.href;
+    acc.push({
+      url: `${siteConfig.url}${href}`,
+      lastModified: new Date(),
+    });
+  });
+  return acc;
+}, []);
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: `${siteConfig.url}/`,
-      lastModified: new Date(),
-    },
-  ];
+  return siteUrls;
 }
