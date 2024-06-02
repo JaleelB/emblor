1045b02bf62e17b846e66f6ae061499a462322fc
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-06-01 23:53:31 -0400

    feat: mdx table components

--- website/components/mdx-components.tsx
+++ website/components/mdx-components.tsx
@@ -10,6 +10,7 @@ import Variants from './mdx/variants';
 import Props from './mdx/props';
 import Demo from './mdx/demo';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 const components = {
   h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
@@ -59,26 +60,38 @@ const components = {
     <img className={cn('rounded-md', className)} alt={alt} {...props} />
   ),
   hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => <hr className="my-4 md:my-8" {...props} />,
-  table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
-    <div className="my-6 w-full overflow-y-auto">
-      <table className={cn('w-full', className)} {...props} />
+  Table: ({ className, ...props }: React.ComponentProps<typeof Table>) => (
+    <div className="rounded-md border">
+      <Table className={cn('w-full min-h-[300px]', className)} {...props} />
     </div>
   ),
-  tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-    <tr className={cn('m-0 border-t p-0 even:bg-muted', className)} {...props} />
+  TableCaption: ({ className, ...props }: React.ComponentProps<typeof TableCaption>) => (
+    <TableCaption className={cn('font-medium text-center', className)} {...props} />
   ),
-  th: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    <th
+  TableHead: ({ className, ...props }: React.ComponentProps<typeof TableHead>) => (
+    <TableHead className={cn('bg-muted/50', className)} {...props} />
+  ),
+  TableBody: ({ className, ...props }: React.ComponentProps<typeof TableBody>) => (
+    <TableBody className={cn('divide-y divide-muted', className)} {...props} />
+  ),
+  TableRow: ({ className, ...props }: React.ComponentProps<typeof TableRow>) => (
+    <TableRow className={cn('even:bg-muted', className)} {...props} />
+  ),
+  TableHeader: ({ className, ...props }: React.ComponentProps<typeof TableHeader>) => (
+    <TableHeader
       className={cn(
-        'border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
+        'border-b px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
         className,
       )}
       {...props}
     />
   ),
-  td: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    <td
-      className={cn('border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right', className)}
+  TableCell: ({ className, ...props }: React.ComponentProps<typeof TableCell>) => (
+    <TableCell
+      className={cn(
+        'border-b px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right',
+        className,
+      )}
       {...props}
     />
   ),
