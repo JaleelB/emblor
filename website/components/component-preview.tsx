117aa82b05bc07fe2fead67ed42287a8b21c9998
Jaleel Bennett <jaleelbennett58@gmail.com>
2024-07-28 15:21:28 -0400

    fix(component-preview): removing disabled prop from component preview

--- website/components/component-preview.tsx
+++ website/components/component-preview.tsx
@@ -186,7 +186,7 @@ export function ComponentPreview({ className, selectOptions, propName, ...otherP
               'items-center': true,
             })}
           >
-            <TagInput {...defaultProps} {...props} disabled={true} />
+            <TagInput {...defaultProps} {...props} />
           </div>
         </TabsContent>
         <TabsContent value="code">
